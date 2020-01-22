f8b9363c3afc5f29109d16159589aead3914adad
Fikri Karim <fkfikrikarim@gmail.com>
2020-01-22 14:18:52 +0700

    Make the list for each Commit on RepoDetailsScreen

--- screens/RepoDetailsScreen.js
+++ screens/RepoDetailsScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, List, Avatar } from "react-native-paper";
 import { getCommits } from "../api";
 
 const RepoDetailsScreen = props => {
@@ -17,8 +17,19 @@ const RepoDetailsScreen = props => {
 
   const Commit = ({ item }) => {
     const { message } = item.commit;
+    // Why it's possible for a commit not to have author?
+    // This 9457efa84c872f029027cdcfc3bae4f403715e48 commit on react-native
+    const username = item.author ? item.author.login : "";
+    // https://stackoverflow.com/questions/22932422
+    const imageURL = `https://github.com/${username}.png`;
 
-    return <Text>{message}</Text>;
+    return (
+      <List.Item
+        title={username}
+        description={message}
+        left={() => <Avatar.Image source={{ uri: imageURL }} />}
+      />
+    );
   };
 
   return (
