477c207d3fe15f4a4ca43c8aa9af67cf57ee9ac2
Fikri Karim <fkfikrikarim@gmail.com>
2020-01-26 21:44:34 +0700

    Remove: expo modules, unimodules, and add react-native-vector-icons

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
-import { AppLoading } from "expo";
-import { Asset } from "expo-asset";
-import * as Font from "expo-font";
-import React, { useState } from "react";
+import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 
@@ -9,45 +6,19 @@ import { theme } from "./constants/theme";
 import AppNavigator from "./navigation/AppNavigator";
 import NavigationService from "./navigation/NavigationService";
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <PaperProvider theme={theme}>
-        <View style={styles.container}>
-          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-          <AppNavigator
-            ref={navigatorRef => {
-              NavigationService.setTopLevelNavigator(navigatorRef);
-            }}
-          />
-        </View>
-      </PaperProvider>
-    );
-  }
-}
-
-async function loadResourcesAsync() {
-  await Promise.all([Asset.loadAsync([]), Font.loadAsync({})]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+        <AppNavigator
+          ref={navigatorRef => {
+            NavigationService.setTopLevelNavigator(navigatorRef);
+          }}
+        />
+      </View>
+    </PaperProvider>
+  );
 }
 
 const styles = StyleSheet.create({
